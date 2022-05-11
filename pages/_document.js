c2595e657b19a1b53be8462ce58b732afd13c78d
venezia-dev <dev.venezia@gmail.com>
2022-05-11 11:58:38 +0100

    Bug fix _document.

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,17 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
