9926250723035c5ba48e4ba2fa17f2e9648f5c86
venezia-dev <dev.venezia@gmail.com>
2022-05-14 17:24:03 +0100

    Add page item.

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -1,16 +1,120 @@
-import { useRouter } from "next/router";
-
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  SimpleGrid,
+  Square,
+  Text,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import ItemCard from '../../components/Cards/ItemCard'
 
 const ItemById = () => {
+  const router = useRouter()
+  const { id } = router.query
 
-    const router = useRouter();
-    const { id } = router.query;
+  const IMAGE =
+    'https://images.unsplash.com/photo-1587089879249-87bf7d2972df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2952&q=80'
 
-    return (
-        <>
+  return (
+    <>
+      <Container maxW="container.xl">
         Item de tal: {id}
-        </>
-    )
+        <Flex width={'full'} direction={{ base: 'column', md: 'row' }}>
+          <Box width={{ base: '100%', md: '66%' }} m="5px">
+            <Box padding="5px">
+              <Center>
+                <Image
+                  alt="Img Item"
+                  rounded={'lg'}
+                  height={'600px'}
+                  width={'full'}
+                  objectFit={'cover'}
+                  src={IMAGE}
+                />
+              </Center>
+            </Box>
+            <Box>
+              <Divider />
+              <Box>
+                <Flex>
+                  <ItemCard />
+                  <ItemCard />
+                  <ItemCard />
+                </Flex>
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            padding="5px"
+            m="5px"
+            width={{ base: '100%', md: '33%' }}
+            h="546px"
+            border={'solid 1px #bababa;'}
+          >
+            <Text>New</Text>
+            <Text>Enlego English Courses</Text>
+            <Text>Sell by Englego</Text>
+            <Text>45 Ubis</Text>
+            <Button
+              bg="#00abd1" color="white"
+            >
+              Buy Now
+            </Button>
+          </Box>
+        </Flex>
+        <Divider />
+        <Box m="1em">
+          <Text mt="10px">Course description</Text>
+          <Text mt="10px">
+            Specialized in personalized spoken English “Conversation” classes,
+            in which our teachers will engage you in discussion of topics of
+            your choice, ranging from politics and economics, science, music,
+            history, culture, sports and anything else that might tickle your
+            fancy. Our conversation classes include reading contemporary
+            articles via Skype, with particular attention to pronunciation, new
+            vocabulary, grammar structures and common expressions of the English
+            language. Our native speaker teachers are reliable professionals
+            willing to go the extra mile to make sure you are satisfied with the
+            quality of conversations you are having!
+          </Text>
+          <Text mt="10px">
+            Our educators tailor-fit their English lessons to individual
+            student’s needs. We focus on developing our students’ communicative
+            proficiency in order to prepare them for real-life encounters with
+            other English speakers. Our insistence on employing only the best
+            English teachers, and following strict recruitment criteria, has
+            ensured quality as the foundation of the system. Teachers guide and
+            support students throughout their studies, providing engaging and
+            interactive English lessons and offering helpful feedback. We make
+            English classes as enjoyable as possible to immerse students in the
+            skills they are practicing and to ensure every student reaches his
+            full potential. Every English lesson is geared towards providing
+            opportunities for learners to use the target structure. Teachers
+            will employ communicative teaching techniques which best suit the
+            age, level, and ability of each student. Other key benefits are
+            accurate and natural pronunciation and intonation as well as
+            appropriation in the use of vocabulary, grammar, and functional
+            language. A combination of these approaches by native-English
+            teachers yields incredible results! Mission We commit to ensure that
+            our English language learners receive rigorous curriculum standards
+            and achieve high levels of academic success Vision Enlego seeks to
+            provide a learning environment which is caring, motivating and
+            supportive allowing all cultures within our school to achieve their
+            personal best and to become excellent English speakers.
+          </Text>
+        </Box>
+      </Container>
+    </>
+  )
 }
 
-export default ItemById;
+export default ItemById
