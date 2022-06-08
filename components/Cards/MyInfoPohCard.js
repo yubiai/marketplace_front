f86b6e08bc8ab3f17674aa3fac49e3d05e8d1d74
Lea <dev.venezia@gmail.com>
2022-06-08 16:24:27 +0100

    Change link external profile.

--- components/Cards/MyInfoPohCard.js
+++ components/Cards/MyInfoPohCard.js
@@ -8,67 +8,77 @@ import {
   Text,
 } from '@chakra-ui/react'
 import Link from 'next/link'
-const MyInfoPohCard = ({dataProfile, balance}) => {
+const MyInfoPohCard = ({ dataProfile, balance }) => {
+  if (!dataProfile)
+    return (
+      <>
+        <Text>No Data</Text>
+      </>
+    )
 
-  if(!dataProfile) return (
-    <>
-      <Text>No Data</Text>
-    </>
-  )
-
-  if(dataProfile) return (
-    <>
-      <Center py={6}>
-        <Stack
-          borderWidth="1px"
-          borderRadius="lg"
-          w={{ sm: '100%', md: '100%' }}
-          height={{ sm: 'full', md: '10rem' }}
-          direction={{ base: 'column', md: 'row' }}
-          bg={'white'}
-          boxShadow={'2xl'}
-          padding={4}
-        >
-          <Flex flex={0.2}>
-            <Image
-              alt="Photo perfil"
-              objectFit="cover"
-              boxSize="100%"
-              borderRadius={'10px'}
-              src={
-                dataProfile.photo
-              }
-            />
-          </Flex>
+  if (dataProfile)
+    return (
+      <>
+        <Center py={6}>
           <Stack
-            flex={1}
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="left"
-            p={1}
-            pt={2}
+            borderWidth="1px"
+            borderRadius="lg"
+            w={{ sm: '100%', md: '100%' }}
+            height={{ sm: 'full', md: '10rem' }}
+            direction={{ base: 'column', md: 'row' }}
+            bg={'white'}
+            boxShadow={'2xl'}
+            padding={4}
           >
-            <Heading fontSize={'2xl'} fontFamily={'body'}>
-              {dataProfile.first_name} {dataProfile.last_name}
-            </Heading>
-            <Text fontWeight={600} color={'gray.500'} size="sm" mb={4}>
-            {dataProfile.eth_address}
-            </Text>
-            <Text fontWeight={600} color={'gray.500'} size="sm" mb={4}>
-              {balance || "No Data"} ubis
-            </Text>
+            <Flex flex={0.2}>
+              <Image
+                alt="Photo perfil"
+                objectFit="cover"
+                boxSize="100%"
+                borderRadius={'10px'}
+                src={dataProfile.photo}
+              />
+            </Flex>
+            <Stack
+              flex={1}
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="left"
+              p={1}
+              pt={2}
+            >
+              <Heading fontSize={'2xl'} fontFamily={'body'}>
+                {dataProfile.first_name} {dataProfile.last_name}
+              </Heading>
+              <Text fontWeight={600} color={'gray.500'} size="sm" mb={4}>
+                {dataProfile.eth_address}
+              </Text>
+              <Text fontWeight={600} color={'gray.500'} size="sm" mb={4}>
+                {balance || 'No Data'} ubis
+              </Text>
+            </Stack>
+            <Flex flex={0.2} justifyContent="center" alignItems="center">
+              <Link
+                href={
+                  'https://app.proofofhumanity.id/profile/' +
+                  dataProfile.eth_address
+                }
+                passHref
+              >
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button bg="#00ABD1" color="white">
+                    My PoH Profile
+                  </Button>
+                </a>
+              </Link>
+            </Flex>
           </Stack>
-          <Flex flex={0.2} justifyContent="center" alignItems="center">
-            <Link href={'https://app.proofofhumanity.id/profile/' + dataProfile.eth_address} isExternal>
-            <Button bg="#00ABD1" color="white">
-              My PoH Profile
-            </Button>
-            </Link>
-          </Flex>
-        </Stack>
-      </Center>
-    </>
-  )
+        </Center>
+      </>
+    )
 }
 
 export default MyInfoPohCard
