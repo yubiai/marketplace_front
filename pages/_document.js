c32630e7b4b443acf621d8c232a01ef5d02c5a3b
Lea <dev.venezia@gmail.com>
2022-06-15 10:59:48 +0100

    Added ga.

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,24 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
         <Head>
-          <script
-            async
+          <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-SZSZPLEC9X"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){window.dataLayer.push(arguments)}
-                  gtag("js", new Date());
-                  gtag("config", "G-SZSZPLEC9X");
-              `,
-            }}
-          ></script>
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SZSZPLEC9X');
+        `}
+          </Script>
         </Head>
         <body>
           <Main />
