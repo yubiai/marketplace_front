13721792a76f1ec9c97e1da4490a70f991003c6b
Lea <dev.venezia@gmail.com>
2022-06-15 14:19:23 +0100

    Format date order.

--- components/Cards/OrderCard.js
+++ components/Cards/OrderCard.js
@@ -1,4 +1,5 @@
 import { Stack, Text, Button } from '@chakra-ui/react'
+import moment from 'moment'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FcLock } from 'react-icons/fc'
@@ -20,7 +21,7 @@ const OrderCard = ({ order }) => {
         justifyContent="space-between"
       >
         <Text fontSize={{ base: 'sm' }} textAlign={'left'} maxW={'4xl'}>
-          Date: {order.dateOrder}
+          Date: {order.createdAt ? moment(order.createdAt).format('MM/DD/YYYY') : moment(order.dateOrder).format('MM/DD/YYYY')}
         </Text>
         <Text fontSize={{ base: 'sm' }} maxW={'4xl'}>
           Item: <Link href={`/item/${order.items[0].slug}`}>{order.items[0].title}</Link>
