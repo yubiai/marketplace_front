13c2e2ee2fd9693ccb3abe46d29117adfce75046
Lea <dev.venezia@gmail.com>
2022-06-15 14:54:02 +0100

    Bug fix card order.

--- components/Cards/OrderCard.js
+++ components/Cards/OrderCard.js
@@ -6,14 +6,13 @@ import { FcLock } from 'react-icons/fc'
 
 const OrderCard = ({ order }) => {
   const router = useRouter()
-  console.log(order)
 
   return (
     <Stack p="4" boxShadow="lg" m="4" borderRadius="sm">
       <Stack direction="row" alignItems="center">
         <Text fontWeight="semibold">Order</Text>
         <FcLock />
-        <Text>{order.status}</Text>
+        <Text>{order?.status}</Text>
       </Stack>
 
       <Stack
@@ -21,10 +20,10 @@ const OrderCard = ({ order }) => {
         justifyContent="space-between"
       >
         <Text fontSize={{ base: 'sm' }} textAlign={'left'} maxW={'4xl'}>
-          Date: {order.createdAt ? moment(order.createdAt).format('MM/DD/YYYY') : moment(order.dateOrder).format('MM/DD/YYYY')}
+          Date: {order.createdAt ? moment(order?.createdAt).format('MM/DD/YYYY') : moment(order?.dateOrder).format('MM/DD/YYYY')}
         </Text>
         <Text fontSize={{ base: 'sm' }} maxW={'4xl'}>
-          Item: <Link href={`/item/${order.items[0].slug}`}>{order.items[0].title}</Link>
+          Item: <Link href={`/item/${order?.items[0]?.slug}`}>{order?.items[0]?.title}</Link>
         </Text>
         <Stack direction={{ base: 'column', md: 'row' }}>
           <Button
@@ -34,14 +33,14 @@ const OrderCard = ({ order }) => {
             ml="1em"
             cursor={'pointer'}
             display={{ base: 'none', md: 'flex' }}
-            onClick={() => router.push('/profile/orders/detail/' + order.transactionHash)}
+            onClick={() => router.push('/profile/orders/detail/' + order?.transactionHash)}
           >
             Detail
           </Button>
         </Stack>
       </Stack>
       <Text fontSize={{ base: 'sm' }} maxW={'4xl'}>
-          Seller: {order.items[0].seller.first_name} {order.items[0].seller.last_name} (Wallet: {order.items[0].seller.eth_address}) 
+          Seller: {order?.items[0]?.seller?.first_name} {order?.items[0]?.seller?.last_name} (Wallet: {order?.items[0]?.seller?.eth_address}) 
         </Text>
     </Stack>
   )
