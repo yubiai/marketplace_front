4a9f348e193f8439f78c57e998d5b40258b91b6d
Lea <dev.venezia@gmail.com>
2022-06-23 11:08:18 +0100

    Bug fix.

--- pages/help.js
+++ pages/help.js
@@ -1,4 +1,4 @@
-import { Box, Center, Container, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 
 const Help = () => {
