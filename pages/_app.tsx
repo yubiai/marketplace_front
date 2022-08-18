9eaca15fed3503985ad1869e0193bff34b4ace9e
venezia-dev <dev.venezia@gmail.com>
2022-08-18 13:12:42 -0300

    Color mode dark

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,12 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import { extendTheme } from '@chakra-ui/react'
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
 }
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ config })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
