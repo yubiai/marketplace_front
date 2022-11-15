520765207ac76b666284062cc67c7a6c92c2f3cd
David Rivero <david.davidsociety.rivero@gmail.com>
2022-11-15 23:20:32 +0000

    fix: Display actions on seller view.

--- components/Buttons/ButtonStartClaim.js
+++ components/Buttons/ButtonStartClaim.js
@@ -5,7 +5,7 @@ const ButtonStartClaim = ({ transactionMeta, isSeller }) => {
     return (
         <Link href={`/profile/evidences/new/${transactionMeta.transactionHash}`}
               size="sm" bg="green.500" color="white" _hover={{ bg: "gray.400" }}
-              borderRadius="0.375rem" height="40px" width="120px"
+              borderRadius="0.375rem" height="40px" width={isSeller ? "200px" : "120px"}
               textAlign="center" fontWeight="bold"
               display="flex" justifyContent="center" alignItems="center">
             { isSeller ? "Challenge claim" : "Start claim" }
