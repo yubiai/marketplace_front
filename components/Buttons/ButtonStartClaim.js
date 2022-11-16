79bf8eb82f3e1a31d4f8c06b45caf76bae1d7bd1
David Rivero <david.davidsociety.rivero@gmail.com>
2022-11-16 23:07:50 +0000

    feat: Add challenge claim CTA.

--- components/Buttons/ButtonStartClaim.js
+++ components/Buttons/ButtonStartClaim.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from '@chakra-ui/react';
 
-const ButtonStartClaim = ({ transactionMeta, isSeller }) => {  
+const ButtonStartClaim = ({ transactionMeta }) => {  
     return (
         <Link href={`/profile/evidences/new/${transactionMeta.transactionHash}`}
               size="sm" bg="green.500" color="white" _hover={{ bg: "gray.400" }}
-              borderRadius="0.375rem" height="40px" width={isSeller ? "200px" : "120px"}
+              borderRadius="0.375rem" height="40px" width="120px"
               textAlign="center" fontWeight="bold"
               display="flex" justifyContent="center" alignItems="center">
-            { isSeller ? "Challenge claim" : "Start claim" }
+            Start claim
         </Link>
     );
 };
