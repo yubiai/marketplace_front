dee28b2d3309777e470ab763b259f615c98d7cd6
venezia-dev <dev.venezia@gmail.com>
2022-11-24 20:18:45 -0300

    Change border radius card

--- components/Cards/OrderCardSeller.js
+++ components/Cards/OrderCardSeller.js
@@ -8,7 +8,7 @@ const OrderCardSeller = ({ order }) => {
   const router = useRouter()
 
   return (
-    <Stack p="4" boxShadow="lg" m="0.5px" borderRadius="sm" bg="white">
+    <Stack p="4" boxShadow="lg" m="0.5px" borderRadius="lg" bg="white">
 
       <Stack direction={{ base: "column", md: "row" }} alignItems="left">
         <Text fontWeight="semibold">
