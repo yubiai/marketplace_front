34bbb118a83a0bf773a95539ebd64b58e326101b
venezia-dev <dev.venezia@gmail.com>
2023-01-10 23:14:02 -0300

    Test ver ethers

--- components/Layouts/Questions.js
+++ components/Layouts/Questions.js
@@ -102,9 +102,6 @@ const Questions = ({ item, profile_id, token }) => {
         return
     }
 
-    console.log(questions, "questions")
-    console.log(profile_id, "Profile_id")
-
     return (
         <>
             {!profile_id || item && item.seller && item.seller._id !== profile_id && (
