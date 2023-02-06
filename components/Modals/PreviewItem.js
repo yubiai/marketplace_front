e7bcb7e8c618f2ea793587fc47fdc19998eff607
Ignacio Britos <igbrityub@air-de-ignacio.home>
2023-02-05 22:46:52 -0300

    feat: added last esp trans to listing modal

--- components/Modals/PreviewItem.js
+++ components/Modals/PreviewItem.js
@@ -3,9 +3,11 @@ import { useState } from 'react'
 import ImagePreviewListingCard from '../Cards/ImagePreviewListingCard'
 import PlayerAudio from '../Utils/PlayerAudio'
 import PlayerVideo from '../Utils/PlayerVideo'
+import { useTranslation } from "react-i18next";
 
 const PreviewItem = ({ item }) => {
   const [selectFile, setSelectFile] = useState(item && item.files[0])
+  const { t } = useTranslation("newlisting");
 
   return (
     <Flex width={'full'} direction={{ base: 'column', sm: 'column', md: 'row' }}>
@@ -47,19 +49,19 @@ const PreviewItem = ({ item }) => {
         </Box>
       </Box>
       <Box width={{base: "100%", sm: "100%", md: "50%"}} p={'1em'}>
-        <Text fontWeight={'bold'}>Title</Text>
+        <Text fontWeight={'bold'}>{t("Title")}</Text>
         <Text>{item?.title}</Text>
-        <Text fontWeight={'bold'}> Description: </Text>
+        <Text fontWeight={'bold'}>{t("Description")}</Text>
         <Text>{item?.description}</Text>
-        <Text fontWeight={'bold'}>Category: </Text>
+        <Text fontWeight={'bold'}>{t("Category")}</Text>
         <Text>{item?.category}</Text>
-        <Text fontWeight={'bold'}>Sub Category: </Text>
+        <Text fontWeight={'bold'}>{t("Sub Category")} </Text>
         <Text>{item?.subcategory}</Text>
-        <Text fontWeight={'bold'}>Net Price: </Text>
+        <Text fontWeight={'bold'}>{t("Net Price")} </Text>
         <Text>{item?.price} {item?.currencySymbolPrice}</Text>
-        <Text fontWeight={'bold'}>UBI Burning Amount: </Text>
+        <Text fontWeight={'bold'}>{t("UBI Burning Amount")} </Text>
         <Text> {item?.ubiburningamount} % ({item?.price * item?.ubiburningamount / 100} {item?.currencySymbolPrice})</Text>
-        <Text fontWeight={'bold'}>Total to receive: </Text>
+        <Text fontWeight={'bold'}>{t("Total to receive")} </Text>
         <Text>{item?.price - item?.price * item?.ubiburningamount / 100} {item?.currencySymbolPrice}</Text>
 
 
