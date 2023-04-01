2575ead8737a0a2349cd7f83729a3c67b762ee83
venezia-dev <dev.venezia@gmail.com>
2023-04-01 17:13:48 -0300

    Deploy produ

--- pages/404.js
+++ pages/404.js
@@ -25,7 +25,7 @@ export default function Custom404() {
           Page Not Found
         </Text>
         <Text color={'gray.500'} mb={6}>
-          Page not exists
+          Page not exists.
         </Text>
 
         <Button bg="#00abd1" color="white" variant="solid">
