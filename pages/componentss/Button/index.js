091e966e9cc66fbc18fd4af8be5888cf87b7a311
venezia-dev <dev.venezia@gmail.com>
2023-06-17 17:46:47 -0300

    Bug fix build

--- pages/componentss/Button/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { Box } from '@chakra-ui/react'
-
-export const Button = ({ children, ...props  }) => (
-  <Box as="button" {...props} bg="red">
-    {children}
-  </Box>
-)
