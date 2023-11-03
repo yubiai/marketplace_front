5e808e2e8f488e13c99cb8cd608b17ea4e8d059c
venezia-dev <dev.venezia@gmail.com>
2023-11-03 14:00:48 -0300

    Change maintenance

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const nextTranslate = require('next-translate')
 
 module.exports = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "1" ? {
-  redirects() {
-    return [{ source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }].filter(Boolean);
+  async redirects() {
+    return [{ source: "/((?!maintenance).*)", destination: "/maintenance.html", permanent: false }]
   }
 } : nextTranslate({
   // ... your existing config
