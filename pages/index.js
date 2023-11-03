45c5c879f6a7ca117aee70cf399ba70344755647
venezia-dev <dev.venezia@gmail.com>
2023-11-03 12:39:47 -0300

    Change id categories

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ const Home = ({ items }) => {
 
 export async function getStaticProps() {
   try {
-    const res = await axios.get('/items/?size=30&categoryId=652366200f5cd11f8c09800d')
+    const res = await axios.get(`/items/?size=30&categoryId=${process.env.NEXT_PUBLIC_ENV == "prod" ? "628be6c99659a661e05f9e2f" : "652366200f5cd11f8c09800d"}`)
     const items = res.data.items;
     if (items.length === 0) {
       return { notFound: true };
